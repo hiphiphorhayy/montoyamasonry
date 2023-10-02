a7b11dd1539273859d33295b0a68c57cc78d2ee7
George <mgeor379@outlook.com>
2023-10-02 18:56:56 -0400

    Delete CORS package & Implementation

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import cors from 'cors';
 import mysql from 'mysql2';
 import { body, validationResult } from 'express-validator';
 const port = process.env.PORT || 3000;
 const app = express();
-const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
 dotenv.config();
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
